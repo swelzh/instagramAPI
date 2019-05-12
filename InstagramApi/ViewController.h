12266f3298c41599b5571d4d8f0d50ad193aa5a8
lizhihua495 <lizhihua495@pingan.com.cn>
2019-05-12 15:13:40 +0800

    Initial Commit

---
+++ InstagramApi/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  InstagramApi
+//
+//  Created by swelzh on 2019/5/12.
+//  Copyright © 2019 swelzh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ InstagramApi/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  InstagramApi
+//
+//  Created by swelzh on 2019/5/12.
+//  Copyright © 2019 swelzh. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end
